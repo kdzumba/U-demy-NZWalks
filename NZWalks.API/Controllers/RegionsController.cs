273816ee2f632c57d7925ace6c9a60b2fdeacb16
Knowledge <knowledged@bbd.co.za>
2023-02-04 23:26:19 +0200

    repository pattern with DTO and AutoMapper

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers;
+
+public class RegionsController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
